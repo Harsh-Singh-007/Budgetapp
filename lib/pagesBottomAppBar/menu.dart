9c0729991b773cdb8ba6970d565f7381dce07a5f
Harsh Singh <sharsh6111@gmail.com>
2022-02-19 02:36:15 +0530

    More Pages

--- lib/pagesBottomAppBar/menu.dart
+++ lib/pagesBottomAppBar/menu.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MenuList extends StatelessWidget {
@@ -6,10 +7,58 @@ class MenuList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Category Settings")),
-      body: Container(
+      body: SafeArea(
         child: Container(
-          color: Colors.blue,
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 120,
+                width: 410,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                          onPressed: () => Navigator.pop(context, false),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            "Category Setting",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0, left: 50),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Expenses",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          SizedBox(
+                            width: 120,
+                          ),
+                          Text(
+                            "Income",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
